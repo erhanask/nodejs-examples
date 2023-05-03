3befae6cf752328d2168b80b9cffb61b0cb865b3
erhanask <erhanasik8@gmail.com>
2023-05-03 21:24:03 +0300

    added pages.

--- video-site-with-express-example/app.js
+++ video-site-with-express-example/app.js
@@ -2,15 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const logger = (req, res, next) => {
+    console.log('visited');
+    next();
+}
+// Setting static files
+app.use("/public",express.static(__dirname+'/public'))
+app.use(logger);
 
-    const photo = {
-        id: 1,
-        name: "Photo Name",
-        description: "Photo description"
-    }
-    res.send(photo)
+// Setting view engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
 })
+
+app.get("/about", (req, res) => {
+    res.render('about');
+});
+
+app.get("/add_post", (req, res) => {
+    res.render('add_post');
+});
 
 const port = 3000;
 app.listen(port, () => {
