da2e2c874dc8e4822f9f43d950afb1d98110204d
erhanask <erhanasik8@gmail.com>
2023-05-07 21:15:06 +0300

    Added mongoose.

--- video-site-with-express-example/app.js
+++ video-site-with-express-example/app.js
@@ -1,31 +1,55 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const port = 3000;
 const app = express();
+const Photo = require('./models/Photos');
+
 
 const logger = (req, res, next) => {
-    console.log('visited');
+    // Every time a request is made, this middleware will be called
+    // console.log('visited');
     next();
 }
+
+// Connect to MongoDB
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/post-site-db');
+}
+
 // Setting static files
-app.use("/public",express.static(__dirname+'/public'))
-app.use(logger);
+app.use("/public", express.static(__dirname + '/public'))
 
 // Setting view engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
+// Middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(logger);
+
+// Models
+
+// Routes
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+    res.render('index', {photos: photos});
 })
 
 app.get("/about", (req, res) => {
     res.render('about');
 });
 
-app.get("/add_post", (req, res) => {
+app.get("/add-post", (req, res) => {
     res.render('add_post');
 });
 
-const port = 3000;
+app.post("/store-post", async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
+});
+
+
 app.listen(port, () => {
     console.log(`Server started at port ${port} ...`);
 });
